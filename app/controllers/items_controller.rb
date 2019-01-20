d7b8236b2e6734b34715716727523dde34898aa7
junya <junya@live.jp>
2019-01-20 13:33:10 +0900

    notification設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,41 @@
 class ItemsController < ApplicationController
+  before_action :set_shop, :set_size
   
   def index
     
   end
   
   def new
-    render layout: 'item'
+    @item = Item.new
+    @item.images.build
+    render layout: 'mypage'
   end
   
   def create
+
+    item = Item.new(item_params)
     
+    if item.save
+      redirect_to new_shop_item_path, notice:"成功"
+    else
+      redirect_to new_shop_item_path, notice:"失敗"
+    end
+    
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :price, :stock, :item_description, size_ids: [], images_attributes: [:name]).merge(item_num: 10, shop_id: 1, category_id: 1)
+  
+  end
+  
+  def set_shop
+    @shop = Shop.find(1)
+  end
+  
+  def set_size
+    @sizes = Size.all
   end
   
 end

776467900cce4e9497ca08f58a3a90f89e5dff1a
junya <junya@live.jp>
2019-01-20 13:32:17 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :shop
   has_many :images
+  accepts_nested_attributes_for :images
   has_many :item_sizes
   has_many :sizes, through: :item_sizes
 end

9f3f5f73c350212f6662f3b8c0302b75368fc47e
junya <junya@live.jp>
2019-01-21 11:12:43 +0900

    S3へ保存に伴うバケット作成

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,6 @@ require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,10 +11,10 @@ CarrierWave.configure do |config|
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1',
-      bucket: 'upload-item-image'
+      bucket: 'chat-space-jun-test1'
     }
-  end
   
-  config.fog_directory  = 'upload-item-image'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-item-image'
+  
+  config.fog_directory  = 'chat-space-jun-test1'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-jun-test1'
 end
